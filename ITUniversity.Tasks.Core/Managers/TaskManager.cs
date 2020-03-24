52ef2b74b338fa0673c48f083232fa0ec8b1c6bb
Dmitriy Klimachev <klimdv@rubicon-it.ru>
2020-03-24 15:42:42 +0300

    Homework 5

--- ITUniversity.Tasks.Core/Managers/TaskManager.cs
+++ ITUniversity.Tasks.Core/Managers/TaskManager.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 
 using ITUniversity.Tasks.Entities;
-using ITUniversity.Tasks.Stores;
+using ITUniversity.Tasks.Repositories;
+//using ITUniversity.Tasks.Stores;
 
 namespace ITUniversity.Tasks.Managers
 {
     /// <inheritdoc/>
     public class TaskManager : ITaskManager
     {
-        private readonly ITaskStore taskStore;
+        //private readonly ITaskStore taskStore;
+        private readonly ITaskRepository taskRepository;
 
-        public TaskManager(ITaskStore taskStore)
+        public TaskManager(ITaskRepository taskRepository)
         {
-            this.taskStore = taskStore;
+            this.taskRepository = taskRepository;
         }
 
         /// <inheritdoc/>
@@ -22,38 +24,38 @@ namespace ITUniversity.Tasks.Managers
             task.CreationDate = DateTime.Now;
             task.Status = Enums.TaskStatus.New;
 
-            return taskStore.Save(task);
+            return taskRepository.Save(task);
         }
 
         /// <inheritdoc/>
         public TaskBase Create(string subject)
         {
             var task = new TaskBase { Subject = subject };
-            return taskStore.Save(task);
+            return taskRepository.Save(task);
         }
 
         /// <inheritdoc/>
         public TaskBase Get(long id)
         {
-            return taskStore.Get(id);
+            return taskRepository.Get(id);
         }
 
         /// <inheritdoc/>
         public ICollection<TaskBase> GetAll()
         {
-            return taskStore.GetAll();
+            return taskRepository.GetAllList();
         }
 
         /// <inheritdoc/>
         public TaskBase Update(TaskBase task)
         {
-            return taskStore.Update(task);
+            return taskRepository.Update(task);
         }
 
         /// <inheritdoc/>
         public void Delete(long id)
         {
-            taskStore.Delete(id);
+            taskRepository.Delete(id);
         }
     }
 }
